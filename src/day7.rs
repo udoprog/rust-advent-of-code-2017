afd68ffdd7106e9e4cf92833cb52debc4bf087ed
John-John Tedro <udoprog@tedro.se>
2017-12-07 19:36:56 +0100

    Simplify: Day 7

--- src/day7.rs
+++ src/day7.rs
@@ -2,25 +2,32 @@ use std::io::{BufRead, BufReader, Read};
 use failure::Error;
 use std::collections::{HashMap, VecDeque};
 
+#[derive(Debug, Default)]
+struct Node {
+    id: String,
+    weight: i32,
+    parent: Option<String>,
+    children: Vec<String>,
+}
+
+#[derive(Debug)]
 struct Tree {
-    pub parents: HashMap<String, String>,
-    pub children: HashMap<String, Vec<String>>,
-    pub nodes: HashMap<String, i32>,
+    nodes: HashMap<String, Node>,
 }
 
 impl Tree {
     /// Calculate the subtree weight for the given node.
-    pub fn subtree_weight(&self, node: &str) -> i32 {
+    pub fn subtree_weight(&self, node: &Node) -> i32 {
         let mut queue = VecDeque::new();
         queue.push_back(node);
 
         let mut weight = 0i32;
 
         while let Some(node) = queue.pop_front() {
-            weight += self.nodes.get(node).map(|v| *v).unwrap_or(0i32);
+            weight += node.weight;
 
-            if let Some(children) = self.children.get(node) {
-                queue.extend(children.iter().map(|s| s.as_str()));
+            for c in &node.children {
+                queue.push_back(self.nodes.get(c.as_str()).expect("child"));
             }
         }
 
@@ -28,9 +35,9 @@ impl Tree {
     }
 
     /// Find the root node.
-    pub fn find_root(&self) -> Option<&str> {
-        for node in self.nodes.keys() {
-            if self.parents.get(node.as_str()).is_none() {
+    pub fn find_root(&self) -> Option<&Node> {
+        for node in self.nodes.values() {
+            if node.parent.is_none() {
                 return Some(node);
             }
         }
@@ -38,29 +45,15 @@ impl Tree {
         None
     }
 
-    /// Find all child nodes for the given node.
-    pub fn children(&self, node: &str) -> Vec<&str> {
-        let mut children = Vec::new();
-
-        if let Some(c) = self.children.get(node) {
-            children.extend(c.iter().map(|s| s.as_str()));
-        }
-
-        children
-    }
-
-    /// Find the weight for the given node.
-    pub fn weight(&self, node: &str) -> Option<i32> {
-        self.nodes.get(node).map(|v| *v)
-    }
-
     /// Find the outlier child node.
     ///
     /// This assumes all child nodes have the same weight except the outlier.
-    pub fn find_outlier(&self, node: &str) -> Option<(&str, i32)> {
-        let mut weights: HashMap<i32, Vec<&str>> = HashMap::new();
+    pub fn find_outlier(&self, node: &Node) -> Option<(&Node, i32)> {
+        let mut weights: HashMap<i32, Vec<&Node>> = HashMap::new();
 
-        for c in self.children(node) {
+        for c in &node.children {
+            let c = self.nodes.get(c.as_str()).expect("node");
+
             weights
                 .entry(self.subtree_weight(c))
                 .or_insert_with(Vec::new)
@@ -85,47 +78,34 @@ impl Tree {
 }
 
 pub fn run<R: Read>(reader: R) -> Result<(String, i32), Error> {
-    let mut data = String::new();
-    let mut reader = BufReader::new(reader);
+    let mut nodes: HashMap<String, Node> = HashMap::new();
 
-    let mut parents = HashMap::new();
-    let mut children = HashMap::new();
-    let mut nodes = HashMap::new();
+    for line in BufReader::new(reader).lines() {
+        let line = line?;
 
-    while reader.read_line(&mut data)? > 0 {
-        {
-            let mut line = data.trim().split(" -> ");
-            let mut left = line.next().expect("lhs").split(" ");
+        let mut line = line.trim().split(" -> ");
+        let mut left = line.next().expect("lhs").split(" ");
 
-            let name = left.next().expect("a name");
-            let number = left.next().expect("a number");
-            let number: i32 = number[1..number.len() - 1].parse()?;
+        let name = left.next().expect("a name");
+        let number = left.next().expect("a number");
+        let number: i32 = number[1..number.len() - 1].parse()?;
 
-            let targets = if let Some(targets) = line.next() {
-                targets.split(", ").collect::<Vec<_>>()
-            } else {
-                vec![]
-            };
+        let targets: Vec<&str> = line.next().iter().flat_map(|t| t.split(", ")).collect();
 
-            nodes.insert(name.to_string(), number);
-
-            for target in targets {
-                parents.insert(target.to_string(), name.to_string());
-                children
-                    .entry(name.to_string())
-                    .or_insert_with(Vec::new)
-                    .push(target.to_string());
-            }
+        for t in &targets {
+            let n = nodes.entry(t.to_string()).or_insert_with(Node::default);
+            n.parent = Some(name.to_string());
         }
 
-        data.clear();
+        let n = nodes.entry(name.to_string()).or_insert_with(Node::default);
+        n.id = name.to_string();
+        n.weight = number;
+        n.children.extend(
+            targets.into_iter().map(|s| s.to_string()),
+        );
     }
 
-    let tree = Tree {
-        parents: parents,
-        children: children,
-        nodes: nodes,
-    };
+    let tree = Tree { nodes: nodes };
 
     let root = tree.find_root().expect("root node");
 
@@ -137,8 +117,7 @@ pub fn run<R: Read>(reader: R) -> Result<(String, i32), Error> {
         diff = d;
     }
 
-    let actual = tree.weight(node).expect("node weight");
-    Ok((root.to_string(), actual + diff))
+    Ok((root.id.to_string(), node.weight + diff))
 }
 
 #[cfg(test)]
@@ -149,7 +128,7 @@ mod tests {
     static INPUT: &str = include_str!("../input/day7.txt");
 
     #[test]
-    fn test_part1() {
+    fn test_all() {
         assert_eq!(
             ("veboyvy".to_string(), 749),
             run(Cursor::new(INPUT)).unwrap()
